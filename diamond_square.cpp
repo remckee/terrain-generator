0a414a346384a8b161c206224b53e403ec7d8a16
remckee <mckeever@oregonstate.edu>
2021-11-30 02:51:25 -0600

    initial commit

---
+++ diamond_square.cpp
@@ -0,0 +1,11 @@
+/*********************
+Name: Rebecca Mckeever
+Course: CS 450
+Final Project
+**********************/
+
+#include "diamond_square.h"
+
+
+
+
---
+++ diamond_square.h
@@ -0,0 +1,19 @@
+/*********************
+Name: Rebecca Mckeever
+Course: CS 450
+Final Project
+**********************/
+
+#ifndef DIAMOND_SQUARE_H
+#define DIAMOND_SQUARE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+
+
+
+#endif /* DIAMOND_SQUARE_H */
+
