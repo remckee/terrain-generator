483b8f43fa6c5eedfde748a28412cb9c28601be6
remckee <mckeever@oregonstate.edu>
2021-12-06 18:00:27 -0600

    reorganized code, fixed inconsistent code style

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -o final glslprogram.cpp mckeever_final.cpp -lGLEW -lGL  -lGLU  /usr/lib/x86_64-linux-gnu/libglut.so  -lm
+g++ -o final glslprogram.cpp diamond_square.cpp osusphere.cpp main.cpp mckeever_final.cpp -lGLEW -lGL  -lGLU  /usr/lib/x86_64-linux-gnu/libglut.so  -lm
 
 g++ -o test1 test.cpp diamond_square.cpp -lGLEW -lGL  -lGLU  /usr/lib/x86_64-linux-gnu/libglut.so  -lm
